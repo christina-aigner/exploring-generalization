eba438955753ca810168ed360ce4e9684604fff0
christina-aigner <christina@luminovo.ai>
2019-05-18 14:41:26 +0200

    Added dependency setup and base implementation

---
+++ src/__init__.py
@@ -0,0 +1,28 @@
+from contextlib import contextmanager
+
+import torch
+
+# Root context is cpu device
+_context_stack = [torch.device("cpu")]
+
+
+@contextmanager
+def device(name: str):
+    """Creates a device context to run in.
+
+    Args:
+        name: pytorch name of the device, e.g. "cpu" or "cuda:0"
+
+    """
+    _context_stack.append(torch.device(name))
+    yield
+    _context_stack.pop()
+
+
+def get_device() -> torch.device:
+    """Gets the current torch device.
+
+    Returns: the torch device that is currently active
+
+    """
+    return _context_stack[-1]
